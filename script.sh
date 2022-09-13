ae48cc63c027706d12322fa89d87862dc53770af
imcalled <57717234+imcalled@users.noreply.github.com>
2022-09-13 14:25:44 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-echo "I am being run from a script"
-touch file1.txt file2.txt file3.txt
-echo "Created file1.txt, file2.txt & file3.txt"
-echo "Listing current directory contents"
-ls
+echo "webhook"
